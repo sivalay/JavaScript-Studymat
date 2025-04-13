142dfc42ed2bf9450efdaea49ce18aaadbcb4eea
Siva <sivalayavinod25@gmail.com>
2025-04-13 18:38:29 +0530

    drawing-app

--- weather-app/script.js
+++ weather-app/script.js
@@ -2,8 +2,7 @@ const apikey = "a";
 const main = document.querySelector('#main');
 const form = document.querySelector('#form');
 const search = document.querySelector('#search');
-console.log(main)
-console.log(form)
+console.log(search)
 
 const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
